4ae1ee1c4e905b1e2dea40c903884046d0aeac59
hobossa <hoboss@gmail.com>
2021-11-04 23:03:15 -0700

    RTK Query for SinglePostPage and getPost

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,31 +1,45 @@
-import { useSelector } from "react-redux";
-import { selectPostById } from "./postsSlice";
 import { Link } from "react-router-dom";
 import { PostAuthor } from "./PostAuthor";
+import { Spinner } from "../../components/Spinner";
+import { useGetPostQuery } from "../api/apiSlice";
+import { TimeAgo } from "./TimeAgo";
+import { ReactionButtons } from "./ReactionButtons";
 
 
-export function SinglePostPage({match}) {
+export function SinglePostPage({ match }) {
     // console.log(arguments);
-    const {postId} = match.params;
+    const { postId } = match.params;
 
-    const post = useSelector(state => selectPostById(state, postId));
+    const {
+        data: post,
+        isFetching,
+        isSuccess,
+    } = useGetPostQuery(postId);
 
-    if (!post) {
-        return (
-            <section>
-                <h2>Post not found</h2>
-            </section>
+    let content;
+
+    if (isFetching) {
+        content = <Spinner text="Loading..." />;
+    } else if (isSuccess) {
+        content = (
+            <article className="post">
+                <h2>{post.title}</h2>
+                <div>
+                    <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date} />
+                </div>
+                <p className="post-content">{post.content}</p>
+                <ReactionButtons post={post} />
+                <Link to={`/editPost/${post.id}`} className="button">
+                    Edit Post
+                </Link>
+            </article>
         );
     }
 
     return (
         <section>
-            <h2>{post.title}</h2>
-            <PostAuthor userId={post.user} />
-            <p className="post-content">{post.content}</p>
-            <Link to={`/editPost/${post.id}`} className="button">
-                Edit Post
-            </Link>
+            {content}
         </section>
     );
 }
